e2b61900b8d103fec890463071d816febca92510
Antony Maziero Warken <againplayi7@gmail.com>
2023-10-20 14:14:32 -0300

    cap 3 finalizado

--- src/telas/Carrinho/index.js
+++ src/telas/Carrinho/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { FlatList } from 'react-native';
 
 import TelaPadrao from '../../componentes/TelaPadrao';
+import StatusCarrinho from '../../componentes/StatusCarrinho';
 import Item from './Item';
 
 const servicos = [
@@ -10,24 +11,30 @@ const servicos = [
     id: 1,
     nome: "Banho",
     preco: 79.9,
-    descricao: "NÃO DE BANHO NO SEU GATO! Mas se precisar nós damos."
+    descricao: "NÃO DE BANHO NO SEU GATO! Mas se precisar nós damos.",
+    quantidade: 1
   },
   {
     id: 2,
     nome: "Vacina V4",
     preco: 89.9,
-    descricao: "Uma dose da vacina V4. Seu gato precisa de duas."
+    descricao: "Uma dose da vacina V4. Seu gato precisa de duas.",
+    quantidade: 2
   },
   {
     id: 3,
     nome: "Vacina Antirrábica",
     preco: 99.9,
-    descricao: "Uma dose da vacina antirrábica. Seu gato precisa de uma por ano."
+    descricao: "Uma dose da vacina antirrábica. Seu gato precisa de uma por ano.",
+    quantidade: 1
   }
 ];
 
-export default function Servicos() {
+export default function Carrinho() {
+  const total = servicos.reduce((soma, {preco, quantidade}) => soma + (preco * quantidade), 0);
+
   return <TelaPadrao>
+    <StatusCarrinho total={total} />
     <FlatList
       data={servicos}
       renderItem={({item}) => <Item {...item}  />}
